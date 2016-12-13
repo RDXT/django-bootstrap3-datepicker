468dc7f82bd4277129cab9951a4680961b2141b7
Scott Adams <scottadams80@gmail.com>
2016-12-13 07:10:00 +0100

    version

--- b3datepicker/__init__.py
+++ b3datepicker/__init__.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-VERSION = (1, 0, 0)
+VERSION = (1, 0, 1)
 __version__ = '.'.join(map(str, VERSION if VERSION[-1] else VERSION[:2]))
 
 default_app_config = 'b3datepicker.conf.B3datepickerConfig'
