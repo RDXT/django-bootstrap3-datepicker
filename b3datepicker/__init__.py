8e2aa51cb88e067c8b80828a56d962c180f19d90
Scott Adams <scottadams80@gmail.com>
2016-12-13 23:19:48 +0100

    duplicate attrs causing probs

--- b3datepicker/__init__.py
+++ b3datepicker/__init__.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-VERSION = (1, 0, 1)
+VERSION = (1, 0, 2)
 __version__ = '.'.join(map(str, VERSION if VERSION[-1] else VERSION[:2]))
 
 default_app_config = 'b3datepicker.conf.B3datepickerConfig'
